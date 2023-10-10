560e47a2eb2dc42c06d1b186e1e03039fa772c4f
Victor Vasquez <victorbrillos1@gmail.com>
2023-10-10 14:07:13 -0400

    no real changes, only setting up the repo with things like gitignore,prettier,etc

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,25 @@ import { Button } from "react-bootstrap";
 //import { ShowHideTasks } from "./tasks/ShowHideTasks";
 
 function App(): JSX.Element {
-    const [quizzer, setQuizzer] = useState<boolean>(true);
+  const [quizzer, setQuizzer] = useState<boolean>(true);
 
-    return (
-        <div className="App">
-            <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
-                <Button
-                    onClick={() => {
-                        setQuizzer(!quizzer);
-                    }}
-                >
-                    {quizzer ? "Quizzer" : "Tasks"}
-                </Button>
-            </header>
-            <div style={{ display: quizzer ? "block" : "none" }}>
-                <Quizzer></Quizzer>
-            </div>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        UD CISC275 with React Hooks and TypeScript
+        <Button
+          onClick={() => {
+            setQuizzer(!quizzer);
+          }}
+        >
+          {quizzer ? "Quizzer" : "Tasks"}
+        </Button>
+      </header>
+      <div style={{ display: quizzer ? "block" : "none" }}>
+        <Quizzer></Quizzer>
+      </div>
+    </div>
+  );
 }
 
 export default App;
